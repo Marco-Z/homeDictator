045e56be54a9f8d6471c27a36b72e67ac9d7f4d4
Marco-Z <marcozugliani94@live.com>
2017-02-22 21:27:41 +0100

    deleted db

--- run.py
+++ run.py
@@ -2,11 +2,7 @@ from bin.app import app
 from homeDictator.db import db_manager
 from datetime import date
 
-# mydb = db_manager()
-# mydb.create()
-
-# mydb.insert('testoh', 'landsat', date.today())
-# mydb.insert('testohh', 'lendsat', date.today())
-# mydb.insert('testoh!', 'explosion', date.today())
+mydb = db_manager()
+mydb.create()
 
 app.run()
